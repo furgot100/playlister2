4a9a5b114fb3af86cb8c527e514b84bf515c2b64
Francis Tsang <frtsang40@gmail.com>
2019-12-01 19:48:06 -0800

    users can destroy playlists

--- app.py
+++ app.py
@@ -9,10 +9,6 @@ playlists = db.playlists
 
 app = Flask(__name__)
 
-# playlists = [
-#     { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
-#     { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
-# ]
 
 def video_url_creator(id_lst):
     videos = []
@@ -54,24 +50,21 @@ def playlists_show(playlist_id):
     playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
     return render_template('playlists_show.html', playlist=playlist)
 
-@app.route('/playlists/<playlist_id>', methods=['POST'])
+
+@app.route('/playlists/<playlist_id>')
 def playlists_update(playlist_id):
     """Submit an edited playlist."""
-    video_ids = request.form.get('video_ids').split()
-    videos = video_url_creator(video_ids)
-    # create our updated playlist
     updated_playlist = {
         'title': request.form.get('title'),
         'description': request.form.get('description'),
-        'videos': videos,
-        'video_ids': video_ids
+        'videos': request.form.get('videos').split()
     }
-    # set the former playlist to the new one we just updated/edited
     playlists.update_one(
         {'_id': ObjectId(playlist_id)},
-        {'$set': updated_playlist})
-    # take us back to the playlist's show page
+        {'$set': updated_playlist}
+    )
     return redirect(url_for('playlists_show', playlist_id=playlist_id))
+
 
 @app.route('/playlists/<playlist_id>/edit')
 def playlists_edit(playlist_id):
@@ -79,6 +72,14 @@ def playlists_edit(playlist_id):
     playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
     # Add the title parameter here
     return render_template('playlists_edit.html', playlist=playlist, title='Edit Playlist')
+
+@app.route('/playlists/<playlist_id>/delete', methods=['POST'])
+def playlists_delete(playlist_id):
+    """Delete one playlist."""
+    playlists.delete_one({'_id': ObjectId(playlist_id)})
+    return redirect(url_for('playlists_index'))
+
+
 
 
 if __name__ == '__main__':
